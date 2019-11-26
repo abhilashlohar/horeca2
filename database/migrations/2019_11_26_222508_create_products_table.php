8e58ed4d3b22a0c7396f1b7d95dd76eee51a47b6
Abhilash Lohar <abhilashlohar01@gmail.com>
2019-11-27 01:54:01 +0530

    sdsd

---
+++ database/migrations/2019_11_26_222508_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->bigInteger('sub_category_id')->unsigned();
+            $table->foreign('sub_category_id')->references('id')->on('sub_categories');
+            $table->text('short_description')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('years','YearController');
-Route::resource('courses','CourseController');
-Route::resource('semesters','SemesterController');
-Route::resource('streams','StreamController');
-Route::resource('users','UserController');
-Route::resource('students','StudentController');
-Route::resource('marksheets','MarksheetController');
-// Route::post('students', 'StudentController@search');
-Route::post('/streamlist', 'StreamController@list')->name('streams.list');
